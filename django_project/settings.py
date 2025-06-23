98ce8a41ef867e3cb4d822ba24b7b6195d69ed35
Kelvin Kigen <Kelvinkigenkosgei@gmail.com>
2025-06-23 22:23:40 +0300

    Api testing

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'books.apps.BooksConfig', #new
+    'rest_framework',
+    'apis.apps.ApisConfig',
 ]
 
 MIDDLEWARE = [
